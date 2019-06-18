4536c0b3af03c3e636f7d1d90581771fbf490be7
Jennifer Ross <ross.m.jennifer@gmail.com>
2019-06-18 06:33:17 -0400

    added separate js file; reorganized files

---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+let computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+      let wins = 0;
+      let losses = 0;
+      let guesses = [];
+      let guessesLeft = 9;
+
+      let directionsText = document.getElementById("directions-text");
+      let winsText = document.getElementById("wins-text");
+      let lossesText = document.getElementById("losses-text");
+      let guessesLeftText = document.getElementById("guesses-text");
+      let userChoiceText = document.getElementById("userchoice-text");
+
+      function reset() {
+        guessesLeft = 9;
+        guesses = [];
+      }
+
+      document.onkeyup = function(event) {
+        let userGuess = event.key;
+        guesses.push(userGuess);
+        let computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+          if (userGuess === computerGuess) {
+            wins++;
+            alert("Feel the power!");
+            reset();
+          } else {
+            guessesLeft--;
+          }
+
+          if (guessesLeft === 0) {
+            losses++;
+            alert("You can't fool Miss Cleo!")
+            reset();
+          }
+
+          directionsText.textContent = "";
+
+          winsText.textContent = "Wins: " + wins;
+          lossesText.textContent = "Losses: " + losses;
+          guessesLeftText.textContent = "Guesses left: " + guessesLeft;
+          userChoiceText.textContent = "Your guesses so far: " + guesses.join(', ');         
+        }
